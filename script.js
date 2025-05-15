24f6d308c09e892fe66f23fe94a146964a2f956c
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-14 20:41:14 -0500

    divs change color when moused over

--- script.js
+++ script.js
@@ -2,10 +2,15 @@ const columnDivs = [];
 const rowDivs = [];
 const container = document.querySelector("#container");
 
-
+function sketch (item) {
+    
+}
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
+        square.addEventListener("mouseenter", (e) => {
+            e.target.style.background = "black"
+        })
         square.textContent = "test";
         square.classList = "grid-item";
         container.append(square);
