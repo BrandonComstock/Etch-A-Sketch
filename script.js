4c5bd43ea7ea11afa4c0fa18c788e98ce5a73fa5
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-15 14:07:29 -0500

    gave div elements height and width, works without test text. pixels turn blackm after mouseover

--- script.js
+++ script.js
@@ -8,11 +8,12 @@ function sketch (item) {
 for (let i = 0; i < 16; i++) {
     for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
-        square.addEventListener("mouseenter", (e) => {
-            e.target.style.background = "black"
-        })
-        square.textContent = "test";
         square.classList = "grid-item";
+        // square.textContent = "test";
+        square.addEventListener("mouseover", (e) => {
+            e.target.style.background = "black"
+        });
+
         container.append(square);
     }
 }
