4917508e3b4b3bd906e5c9ddafc4350cde27b538
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-14 20:03:17 -0500

    16 by 16 grid stretches and looks okau

--- script.js
+++ script.js
@@ -3,8 +3,8 @@ const rowDivs = [];
 const container = document.querySelector("#container");
 
 
-for (let i = 0; i <= 16; i++) {
-    for (let j = 0; j <= 16; j++) {
+for (let i = 0; i < 16; i++) {
+    for (let j = 0; j < 16; j++) {
         const square = document.createElement("div");
         square.textContent = "test";
         square.classList = "grid-item";
