813a1cf1650c57a8c2c5935449973b5886452c61
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-14 19:25:00 -0500

    added columns and rows variable, lopped and made 16 divs on screen

---
+++ script.js
@@ -0,0 +1,13 @@
+const columnDivs = [];
+const rowDivs = [];
+const container = document.querySelector("#container");
+
+
+for (let i = 0; i <= 16; i++) {
+    for (let j = 0; j <= 16; j++) {
+        const square = document.createElement("div");
+        square.textContent = "test";
+        container.append(square);
+    }
+}
+
