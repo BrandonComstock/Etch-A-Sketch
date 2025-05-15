9aa5d35f0a6e990132b32ebd0f48433ee4eb6048
BrandonComstock <brandoncomstock40@gmail.com>
2025-05-14 19:41:12 -0500

    added stylesheet, used flexbos made text appear in columns and rows incorrectly

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ for (let i = 0; i <= 16; i++) {
     for (let j = 0; j <= 16; j++) {
         const square = document.createElement("div");
         square.textContent = "test";
+        square.classList = "grid-item";
         container.append(square);
     }
 }
